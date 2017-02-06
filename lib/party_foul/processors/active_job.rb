60998de5ac75617a94b33a4a1daecc0dd20914de
Alexander Zaytsev <alexander@say26.com>
2017-02-06 20:08:52 +0100

    Add ActiveJob processor

---
+++ lib/party_foul/processors/active_job.rb
@@ -0,0 +1,11 @@
+class PartyFoul::Processors::ActiveJob < ApplicationJob
+  queue_as 'party_foul'
+
+  def self.handle(exception, env)
+    perform_later(Marshal.dump(exception).force_encoding('ISO-8859-1').encode('UTF-8'), Marshal.dump(env).force_encoding('ISO-8859-1').encode('UTF-8'))
+  end
+
+  def perform(exception, env)
+    PartyFoul::ExceptionHandler.new(Marshal.load(exception.force_encoding('UTF-8').encode('ISO-8859-1')), Marshal.load(env.force_encoding('UTF-8').encode('ISO-8859-1'))).run
+  end
+end
